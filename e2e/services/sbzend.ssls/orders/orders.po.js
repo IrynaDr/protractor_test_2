74df3829e7972ee50659f9612632c76d4c243e3f
dranovska <dranovskaa@gmail.com>
2020-02-17 01:25:22 +0200

    Updated order_history_page.spec test

--- e2e/services/sbzend.ssls/orders/orders.po.js
+++ e2e/services/sbzend.ssls/orders/orders.po.js
@@ -1,3 +1,5 @@
+const commonHelper = require('../../helpers/commonHelper.js');
+
 class OrdersPage {
 
     //--------------------------------------------------------------------------
@@ -5,12 +7,37 @@ class OrdersPage {
     //--------------------------------------------------------------------------
     constructor() {
 
-        this.btnAllOrders = $(`[ng-click="activeTab = 'all'"]`);
+        this.txtOrder = $('.title.order-id');
+
+        this.btnAllOrdersActive = $('.all-tab.active');
+
+        this.txtFirstOrderNumber = $('[ng-show*="products"]');
+
+        this.txtCreated = $('.item.date-created');
+
+        this.btnStatus = $('.btn-s.warning');
+
+        this.txtAmount = $('.item.amount');
+
+        this.txtItems = $('.item.items');
+
+        this.txtOrderTitleView = $('.tab-item.ng-binding');
+
+        this.txtItemsView = $('.item.terms[ng-if*="certificate"]');
+
+        this.txtAmountView = $('.item.amount[ng-if*="certificate"]');
+
+        this.txtAmountTotal = $('.item.amount.ng-binding');
 
     }
     //--------------------------------------------------------------------------
     // Functions
     //--------------------------------------------------------------------------
+
+    clickFirstOrder() {
+        this.txtFirstOrderNumber.click();
+        commonHelper.visibleWait(this.txtOrderTitleView);
+    }
 
 };
 
--- e2e/test/sbzend.ssls/order_history_page.spec.js
+++ e2e/test/sbzend.ssls/order_history_page.spec.js
@@ -3,7 +3,10 @@ const pageObject    = require('../../services/sbzend.ssls/pages').container.Page
 const homePage      = pageObject.getHomePage();
 const authPage      = pageObject.getAuthorizationPage();
 const ordersPage    = pageObject.getOrdersPage();
-
+let orderNumber, orderNumber2;
+const orderTitle = 'Order #4399126, not completed on 02 Jul 2019';
+const orderItems = '1 × PositiveSSL × 2 yrs';
+const orderAmount = '$9.76';
 
 describe('Order history page.', () => {
 
@@ -25,28 +28,41 @@ describe('Order history page.', () => {
         homePage.clickProfile();
         commonHelper.visibleWait(homePage.btnProfileOpened);
         homePage.clickBtnOrderHistory();
-        commonHelper.visibleWait(ordersPage.btnAllOrders);
+        commonHelper.visibleWait(ordersPage.txtOrder);
     });
 
     it('The "All orders" tab is opened.', () => {
-
+        commonHelper.visibleWait(ordersPage.btnAllOrdersActive);
     });
 
-    it(`Only one order with the following values is displayed:
+    it(`One order with the following values is displayed:
         - Order #: #4399126,
         - Created: 02 Jul '19,
         - Status: Not completed,
         - Amount: $9.76,
-        - Items: PositiveSSL`.', () => {
-
+        - Items: PositiveSSL`, () => {
+        expect(ordersPage.txtFirstOrderNumber.getText()).toEqual(browser.params.firstOrderData.number);
+        expect(ordersPage.txtCreated.getText()).toEqual(browser.params.firstOrderData.created);
+        expect(ordersPage.btnStatus.getText()).toEqual(browser.params.firstOrderData.status);
+        expect(ordersPage.txtAmount.getText()).toEqual(browser.params.firstOrderData.amount);
+        expect(ordersPage.txtItems.getText()).toEqual(browser.params.firstOrderData.items);
     });
 
     it('Open order details.', () => {
-
+        ordersPage.clickFirstOrder();
     });
 
     it('The orders information should be correct.', () => {
+        expect(ordersPage.txtOrderTitleView.getText()).toEqual(orderTitle);
+        expect(ordersPage.txtItemsView.getText()).toEqual(orderItems);
+        expect(ordersPage.txtAmountView.getText()).toEqual(orderAmount);
+        expect(ordersPage.txtAmountTotal.getText()).toEqual(orderAmount);
 
+//        ordersPage.txtOrderTitleView.getText() TODO
+//            .then(function (value) {
+//                orderNumber2 = value.slice(6, 14);
+//            });
+//        expect(orderNumber2).toEqual(browser.params.firstOrderData.number);
     });
 
 });
